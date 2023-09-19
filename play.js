07337f686d5427b51973b6a2c2a131e2aed14696
Camillia Snow <s.camillia@gmail.com>
2023-09-19 15:30:56 -0300

    moved functions into client file

--- play.js
+++ play.js
@@ -1,30 +1,5 @@
 const net = require("net");
-
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "localhost",
-    port: 50541
-    });
-
-// interpret incoming data as text
-conn.setEncoding("utf8");
-
-// successful connection anon. function
-conn.on("connect", () => {
-  console.log("Connected!");
-});
-
-// adding a timeOut if a user idles without inputting anything
-const timedOut = () => {
-  console.log("you ded cuz you idled");
-  conn.end();
-};
-
-let timeOutNum = setTimeout(timedOut, 2000);
-
-return conn;
-};
+const { connect } = require("./client"); 
 
 console.log("Connecting...");
 connect();
