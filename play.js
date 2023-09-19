59c17aa56c1773d2ba30b1097ad80c00479b0f64
Camillia Snow <s.camillia@gmail.com>
2023-09-19 20:44:03 -0300

    added a termination ability

--- play.js
+++ play.js
@@ -1,5 +1,19 @@
 const net = require("net");
 const { connect } = require("./client"); 
 
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.on("data", handleUserInput);
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin;
+};
+
+const handleUserInput = function (key) {
+  if (key === '\u0003')
+  process.exit();
+}
+
 console.log("Connecting...");
 connect();
