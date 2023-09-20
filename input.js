78f26066e97c486a8289f9fb1581c0a980d6deb5
Camillia Snow <s.camillia@gmail.com>
2023-09-20 18:32:44 -0300

    added code to make the heckin' snek move

--- input.js
+++ input.js
@@ -1,6 +1,9 @@
+const net = require("net");
 
+let connection;
 
-const setupInput = function () {
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.on("data", handleUserInput);
   stdin.setRawMode(true);
@@ -12,6 +15,14 @@ const setupInput = function () {
 const handleUserInput = function (key) {
   if (key === '\u0003')
   process.exit();
+if (key === 'w')
+  connection.write("Move: up");
+if (key === 'a')
+  connection.write("Move: left");
+if (key === 's')
+  connection.write("Move: down");
+if (key === 'd')
+  connection.write("Move: right");
 }
 
 module.exports = { setupInput }
