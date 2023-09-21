adef668588f37386bdffb346246a65f48f99fdd2
Camillia Snow <s.camillia@gmail.com>
2023-09-21 19:48:50 -0300

    fixed a spelling error and syntax

---
+++ constants.js
@@ -0,0 +1,15 @@
+
+const IP = "localhost"
+const PORT = "50541"
+const MOVE_UP_KEY = "w"
+const MOVE_DOWN_KEY = "s"
+const MOVE_RIGHT_KEY = "d"
+const MOVE_LEFT_KEY = "a"
+const messages = {
+  "h": "Say: hek",
+  "y": "Say: yis",
+  "n": "Say: noo",
+  "k": "Say: kek",
+};
+
+module.exports = { IP, PORT, MOVE_UP_KEY, MOVE_DOWN_KEY, MOVE_RIGHT_KEY, MOVE_LEFT_KEY, messages };
