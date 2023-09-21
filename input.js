faced2ed763fb187cec81e8fa235815cdd0c88da
Camillia Snow <s.camillia@gmail.com>
2023-09-21 20:02:55 -0300

    linted and updated some syntax

--- input.js
+++ input.js
@@ -1,8 +1,10 @@
+// this file includes user input for the game. so we can have fun eatin' stuff with our snek
 const net = require("net");
+const { MOVE_UP_KEY, MOVE_DOWN_KEY, MOVE_RIGHT_KEY, MOVE_LEFT_KEY, messages } = require('./constants');
 
 let connection;
 
-const setupInput = function (conn) {
+const setupInput = function(conn) {
   connection = conn;
   const stdin = process.stdin;
   stdin.on("data", handleUserInput);
@@ -12,25 +14,19 @@ const setupInput = function (conn) {
   return stdin;
 };
 
-const handleUserInput = function (key) {
+const handleUserInput = function(key) {
   if (key === '\u0003')
-  process.exit();
-if (key === 'w')
-  connection.write("Move: up");
-if (key === 'a')
-  connection.write("Move: left");
-if (key === 's')
-  connection.write("Move: down");
-if (key === 'd')
-  connection.write("Move: right");
-if (key === "h")
-  connection.write("Say: hek");
-if (key === "y")
-  connection.write("Say: yis");
-if (key === "n")
-  connection.write("Say: noo");
-if (key === "k")
-  connection.write("Say: kek");
-}
+    process.exit();
+  if (key === MOVE_UP_KEY)
+    connection.write("Move: up");
+  if (key === MOVE_LEFT_KEY)
+    connection.write("Move: left");
+  if (key === MOVE_DOWN_KEY)
+    connection.write("Move: down");
+  if (key === MOVE_RIGHT_KEY)
+    connection.write("Move: right");
+  if (messages[key])
+    connection.write(messages[key]);
+};
 
-module.exports = { setupInput }
+module.exports = { setupInput };
